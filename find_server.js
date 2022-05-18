810cfaf27c5c5e07656cb9fe1f27c4060431533c
Kadwen <Kadwen01@gmail.com>
2022-05-18 13:16:22 -0400

    fixed missing references removed in error

--- find_server.js
+++ find_server.js
@@ -20,6 +20,17 @@ function recursiveScan(ns, parent, server, target, route) {
 }
 
 export async function main(ns) {
+    const args = ns.flags([["help", false]]);
+    let route = [];
+    let server = args._[0];
+    if (!server || args.help) {
+        ns.tprint("This script helps you find a server on the network and shows you the path to get to it.");
+        ns.tprint(`Usage: run ${ns.getScriptName()} SERVER`);
+        ns.tprint("Example:");
+        ns.tprint(`> run ${ns.getScriptName()} n00dles`);
+        return;
+    }
+
     recursiveScan(ns, '', 'home', server, route);
     for (const i in route) {
         await ns.sleep(500);
@@ -28,6 +39,6 @@ export async function main(ns) {
     }
 }
 
-export function autocomplete(data) {
+export function autocomplete(data, args) {
     return data.servers;
 }
